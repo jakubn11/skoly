52eeda71c24cb5f538e199a2fb8ab7d2a7a5d070
Jakub Nedela <jakub.nedela@centrum.cz>
2020-09-24 18:50:21 +0200

    Mapa

--- resources/views/lokace/lokace.blade.php
+++ resources/views/lokace/lokace.blade.php
@@ -7,15 +7,52 @@
 </div>
 
 <script>
-    // Initialize and add the map
     function initMap() {
-      // The location of Uluru
-      var uluru = {lat: 20, lng: 20};
-      // The map, centered at Uluru
-      var map = new google.maps.Map(
-          document.getElementById('map'), {zoom: 10, center: uluru});
-      // The marker, positioned at Uluru
-      var marker = new google.maps.Marker({position: uluru, map: map});     
+        
+        var options ={
+            zoom: 2,
+            center: {lat:20.2554, lng:20.2154}
+        }
+
+        var map = new
+        google.maps.Map(document.getElementById('map'), options);
+
+        /*
+        var marker = new google.maps.Marker({
+            position: {lat:42.123, lng:-71.054},
+            map:map
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: '<h4>Místo</h4>'
+        });
+
+        marker.addListener('click', function(){
+            infoWindow.open(map, marker);
+        });
+        */
+        addMarker({coords:{lat:42.123, lng:-71.054}});
+        addMarker({coords:{lat: 25.761681, lng:-80.191788},
+                    content: '<h4>Místo</h4>'
+        });
+        
+        function addMarker(props)
+        {
+            var marker = new google.maps.Marker({
+            position:props.coords,
+            map:map
+        });
+            if(props.content)
+            {
+                var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+
+                marker.addListener('click', function(){
+             infoWindow.open(map, marker);
+            }); 
+            }
+        }
     }
         </script>
         
